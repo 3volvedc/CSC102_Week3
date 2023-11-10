85e7232e572ba6df3ffa7141ef512d7cd344a928
3volved_C <105845362+3volvedc@users.noreply.github.com>
2023-11-09 19:50:09 -0500

    Update Secret Message user_input.js

--- user_input.js
+++ user_input.js
@@ -5,6 +5,8 @@ function validateInputs() {
 
     var fullName = first + ' ' + last;
 
+    document.getElementById('message').innerText = "User data successfully stored.";
+
     if (first.length == 0) {
         document.getElementById('message').innerText = "Please enter a first name.";
         return;
@@ -19,6 +21,7 @@ function validateInputs() {
     }
     if (zip.length !== 5 || isNaN(zip)) {
         document.getElementById('message').innerText = "Please enter a valid 5-digit zip code.";
+        return;
     }
-    document.getElementById('message').innerText = "User data successfully added to database!";
+    
 }
