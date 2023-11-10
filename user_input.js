c2618e1a15f4d345a4a70b8f178dfd1b4cafd7ca
3volved_C <105845362+3volvedc@users.noreply.github.com>
2023-11-09 19:37:44 -0500

    Add files via upload
    User Validation

---
+++ user_input.js
@@ -0,0 +1,24 @@
+function validateInputs() {
+    var first = document.getElementById('first').value;
+    var last = document.getElementById('last').value;
+    var zip = document.getElementById('zip').value;
+
+    var fullName = first + ' ' + last;
+
+    if (first.length == 0) {
+        document.getElementById('message').innerText = "Please enter a first name.";
+        return;
+    }
+    if (last.length == 0) {
+        document.getElementById('message').innerText = "Please enter a last name.";
+        return;
+    }
+    if (fullName.length > 20) {
+        document.getElementById('message').innerText = "Full name is too long. Please use a shorter name.";
+        return;
+    }
+    if (zip.length !== 5 || isNaN(zip)) {
+        document.getElementById('message').innerText = "Please enter a valid 5-digit zip code.";
+    }
+    document.getElementById('message').innerText = "User data successfully added to database!";
+}
